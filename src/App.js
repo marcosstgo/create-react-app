42499750e67cd85f363dd486f6e5eea6a9708373
Marcos Santiago <hello@marcossantiago.com>
2019-05-17 10:26:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,26 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <form name="contact" method="POST" data-netlify="true">
+<p>
+  <label>Your Name: <input type="text" name="name" /></label>
+</p>
+<p>
+  <label>Your Email: <input type="email" name="email" /></label>
+</p>
+<p>
+  <label>Your Role: <select name="role[]" multiple>
+    <option value="leader">Leader</option>
+    <option value="follower">Follower</option>
+  </select></label>
+</p>
+<p>
+  <label>Message: <textarea name="message"></textarea></label>
+</p>
+<p>
+  <button type="submit">Send</button>
+</p>
+</form>
           </p>
           <a
             className="App-link"
